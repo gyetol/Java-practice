9a44253898ef349859c2ba2961d19af836ec498f
gyetol <econostein@naver.com>
2019-12-08 18:43:28 +0900

    first java commit

---
+++ learningJavaEasily/src/chap01/Hello.java
@@ -0,0 +1,13 @@
+package chap01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("æ»≥Á!");
+		System.out.println("æ»≥Á" + "!!!");
+		String hello = "æ»≥Á«œººø‰!";
+		System.out.println(hello);
+	}
+
+}
