2c37bc76aa4c3f7df7d689c02127c25c260acac9
gyetol <econostein@naver.com>
2019-12-14 21:32:35 +0900

    chap03 problems & solutions

--- learningJavaEasily/src/chap03/prob06.java
+++ learningJavaEasily/src/chap03/prob06.java
@@ -9,10 +9,33 @@ public class prob06 {
 		Scanner in = new Scanner(System.in);
 		System.out.print("철수:");
 		cheol = in.next();
-		System.out.print("철수:");
+		System.out.print("영희:");
 		young = in.next();
 
 		if (cheol.equals("r")) {
+			if (young.equals("r")) {
+				System.out.println("무승부!");
+			} else if (young.equals("s")) {
+				System.out.println("철수 승!");
+			} else {
+				System.out.println("영희 승!");
+			}
+		} else if (cheol.equals("s")) {
+			if (young.equals("s")) {
+				System.out.println("무승부!");
+			} else if (young.equals("p")) {
+				System.out.println("철수 승!");
+			} else {
+				System.out.println("영희 승!");
+			}
+		} else {
+			if (young.equals("p")) {
+				System.out.println("무승부!");
+			} else if (young.equals("r")) {
+				System.out.println("철수 승!");
+			} else {
+				System.out.println("영희 승!");
+			}
 		}
 	}
 
