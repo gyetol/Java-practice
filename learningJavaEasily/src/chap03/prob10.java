f9dba8587ee0798fffa571158c9dc5648f302a67
gyetol <econostein@naver.com>
2019-12-15 22:17:53 +0900

    chap03 problems & solutions

--- learningJavaEasily/src/chap03/prob10.java
+++ learningJavaEasily/src/chap03/prob10.java
@@ -14,7 +14,23 @@ public class prob10 {
 		}
 	}
 
-	public static boolean isPrime(int num)
-	{
-		boolean tf;
+	public static boolean isPrime(int num) {
+		if (num == 1) {
+			return false;
+		} // 1은 소수가 아니다
+		if (num == 2) {
+			return true;
+		} // 2는 소수
+		if (num % 2 == 0) {
+			return false;
+		} // 2 이외의 짝수는 소수가 아니다
+		for (int i = 3; i * i <= num; i += 2) // 홀수만 검사한다 // num<9일 경우 for문이 한번도 안돌게 되지만, 짝수를 이미 걸렀으므로 4,6,8은 false가 되고
+												// 3,5,7은 true가 된다
+		{
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
+}
