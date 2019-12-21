de8c984b44ef4427b0dce032ebce1e258d26015d
gyetol <econostein@naver.com>
2019-12-22 01:20:59 +0900

    chap05 problems & solutions

--- learningJavaEasily/src/chap05/prob03.java
+++ learningJavaEasily/src/chap05/prob03.java
@@ -5,13 +5,17 @@ public class prob03 {
         double[][] interests={{3.2,3.1,3.2,3.0},{2.9,2.8,2.7,2.6},{2.7,2.6,2.5,2.7}};
         double[] sum1={0.0,0.0,0.0};
         double sum2=0.0;
-
-        for(double sum:sum1)
+        int i=0;
+        for(double[] row: interests)
         {
-            for(double[] rate:interests)
+            for(double factor: row)
             {
-                sum+=rate
+                sum1[i]+=factor;
             }
+            System.out.printf("%d차년도 평균 이자율 = %.2f%%\n",i+1,sum1[i]/4);
+            sum2+=sum1[i];
+            i++;
         }
+        System.out.printf("3년간 평균 이자율 = %.2f%%\n",sum2/3);
     }
 }
