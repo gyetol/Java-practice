3290311b225caf5d8988ee1078533a8ef99cf60a
gyetol <econostein@naver.com>
2019-12-28 16:39:07 +0900

    chap05 problems & solutions

--- learningJavaEasily/src/chap05/prob06.java
+++ learningJavaEasily/src/chap05/prob06.java
@@ -3,8 +3,9 @@ package chap05;
 public class prob06 {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
-        arr=reverse(arr);
-        for(int num:arr)
+        int arr1[];
+        arr1=reverse(arr);
+        for(int num:arr1)
         {
             System.out.print(num);
             System.out.print(" ");
@@ -12,7 +13,8 @@ public class prob06 {
     }
     public static int[] reverse(int[] org)
     {
-        int array[]=org;
+       // int array[]=org;    <- org배열크기만큼 array배열크기를 할당하려고 했으나, 이 실행문은 변수명은 다르나 같은 객체를 공유하게 된다...
+        int array[]=new int[5];
         for(int i=0;i<org.length;i++)
         {
             array[i]=org[org.length-1-i];
@@ -21,4 +23,4 @@ public class prob06 {
     }
 }
 
-//이해가 안가는 군...
+
--- learningJavaEasily/src/chap05/prob08.java
+++ learningJavaEasily/src/chap05/prob08.java
@@ -1,7 +1,26 @@
+/*
 package chap05;
+
+//import org.jetbrains.annotations.NotNull;
 
 public class prob08 {
     public static void main(String[] args) {
-        //명령행인수
+        int first,second;
+        first=Integer.parseInt(args[0]);
+        second=Integer.parseInt(args[1]);
+        char[][] arr1= new char[first][second];
+        char[][] arr2= new char[first][second];
+        for(int i=0;i<first;i++)
+        {
+            for(int j=0;j<second;j++)
+            {
+                if(Math.random()<0.3){arr1[i][j]='*';}
+                else{arr1[i][j]='-';}
+                System.out.print(arr1[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 }
+*/
