d6c0c6f1a796dd57b9def9e367f2cb331f742fa1
gyetol <econostein@naver.com>
2019-12-29 21:28:49 +0900

    chap06 problems & solutions

---
+++ learningJavaEasily/src/chap06/GirlTest.java
@@ -0,0 +1,12 @@
+package chap06;
+
+public class GirlTest {
+    public static void main(String[] args) {
+        Girl g1= new Girl();
+        Girl g2- new GoodGirl();
+        GoodGirl gg= new bestGirl();
+
+
+        gg.show();
+    }
+}
