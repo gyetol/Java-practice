ad1cd277d04114fc93ca263727124563f1dc46f8
gyetol <econostein@naver.com>
2020-01-04 20:25:47 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/prob07/prob01.java
+++ learningJavaEasily/src/prob07/prob01.java
@@ -1,5 +1,7 @@
 package prob07;
 
+import javax.lang.model.util.AbstractAnnotationValueVisitor6;
+
 public class prob01 {
     public static void main(String[] args) {
         Concrete c = new Concrete(100,50);
@@ -10,4 +12,17 @@ public class prob01 {
 abstract class Abstract{
     int i;
     abstract void show();
+    public Abstract(int i){
+        this.i=i;
+    }
+}
+class Concrete extends Abstract{
+    int j;
+    void show(){
+        System.out.println("i = "+i+", j = "+j);
+    }
+    public Concrete(int i,int j){
+        super(i);
+        this.j=j;
+    }
 }
