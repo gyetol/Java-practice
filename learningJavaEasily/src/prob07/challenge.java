d210b3d393b28b3c37acceae74312493c1a7cb45
gyetol <econostein@naver.com>
2020-01-08 21:44:21 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/prob07/challenge.java
+++ learningJavaEasily/src/prob07/challenge.java
@@ -2,11 +2,20 @@ package prob07;
 
 public class challenge {
     public static void main(String[] args) {
-        Countable[] m={new Bird("뻐꾸기"),new Bird("독수리"),new Tree("사과나무"),new Tree("밤나무"),new Tree("자작나무")};
+        Countable[] m={new Bird("뻐꾸기"),new Bird("독수리"),new Tree("사과나무"),new Tree("밤나무")};
 
         for(Countable e : m)
         {
             e.count();
+        }
+        for(int i=0;i<m.length;i++)
+        {
+            if(m[i] instanceof Bird){
+                ((Bird)m[i]).fly();
+            }
+            else{
+                ((Tree)m[i]).ripen();
+            }
         }
     }
 }
