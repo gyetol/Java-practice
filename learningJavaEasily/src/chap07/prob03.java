ca01f5e3cacf5f6f2101f22ed342f996e6584c46
gyetol <econostein@naver.com>
2020-01-10 21:59:41 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob03.java
+++ learningJavaEasily/src/chap07/prob03.java
@@ -1,7 +1,30 @@
 package chap07;
 
+import java.util.Arrays;
+
 public class prob03 {
     public static void main(String[] args) {
+        Book[] b={new Book(15000),new Book(50000),new Book(20000)};
+        System.out.println("정렬 전");
+        for(Book a:b)
+        {
+            a.show();
+        }
+        Arrays.sort(b);
+        System.out.println("정렬 후");
+        for(Book a:b)
+        {
+            a.show();
+        }
+    }
+}
 
+class Book{
+    int price;
+    public Book(int price){
+        this.price=price;
+    }
+    void show(){
+        System.out.println("Book [price="+price+"]");
     }
 }
