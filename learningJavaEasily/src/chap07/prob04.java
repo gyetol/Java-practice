fd8f1138efaa28949f3784f31958cc3579686856
gyetol <econostein@naver.com>
2020-01-15 23:32:48 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob04.java
+++ learningJavaEasily/src/chap07/prob04.java
@@ -2,6 +2,13 @@ package chap07;
 
 public class prob04 {
     public static void main(String[] args) {
+        speak(new Korean());
+        speak(new American());
+    }
+}
+
+class TalkableTest{
+    static void speak(){
 
     }
 }
