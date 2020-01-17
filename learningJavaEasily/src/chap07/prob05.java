19d627c4b3f75c951675325b8b8a244568468506
gyetol <econostein@naver.com>
2020-01-17 20:28:00 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob05.java
+++ learningJavaEasily/src/chap07/prob05.java
@@ -2,6 +2,11 @@ package chap07;
 
 public class prob05 {
     public static void main(String[] args) {
+        Controller[] c={new TV(false),new Radio(true)};
 
+        for (Controller controller : c)
+        {
+            controller.show();
+        }
     }
 }
