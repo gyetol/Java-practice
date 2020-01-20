e17858764be41de292de65a96a903d9c0b4d07a1
gyetol <econostein@naver.com>
2020-01-20 21:01:15 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob07.java
+++ learningJavaEasily/src/chap07/prob07.java
@@ -2,6 +2,15 @@ package chap07;
 
 public class prob07 {
     public static void main(String[] args) {
+        Human.echo();
+
+        Student s =new Student(20);
+        s.print();
+        s.eat();
+
+        Person p = new Person();
+        p.print();
+        p.eat();
 
     }
 }
