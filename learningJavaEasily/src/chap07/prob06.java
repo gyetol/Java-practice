a666028d0aad5febfe84b1c54818402ecae0d3fa
gyetol <econostein@naver.com>
2020-01-22 22:06:16 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob06.java
+++ learningJavaEasily/src/chap07/prob06.java
@@ -2,7 +2,9 @@ package chap07;
 
 public class prob06 {
     public static void main(String[] args) {
-
+        Printable p =new B4();
+        Call c = new Call();
+        c.invoke(p);
     }
 }
 
@@ -15,3 +17,15 @@ class A4 implements Printable{
         System.out.println("A4");
     }
 }
+
+
+class B4 implements Printable{
+    public void b(){
+        System.out.println("B4");
+    }
+}
+
+class Call{
+
+}
+
