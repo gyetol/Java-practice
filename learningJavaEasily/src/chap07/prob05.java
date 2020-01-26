038e905f5ac7b92592a833bedb8ba77bb2be213a
gyetol <econostein@naver.com>
2020-01-26 21:46:07 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob05.java
+++ learningJavaEasily/src/chap07/prob05.java
@@ -10,3 +10,52 @@ public class prob05 {
         }
     }
 }
+
+abstract class Controller{
+    protected boolean power=false;
+    public void show(){
+        if(power==false){
+            System.out.println("제품이 꺼졌습니다.");
+        }
+        else {
+            System.out.println("제품이 켜졌습니다.");
+        }
+    }
+    public abstract String getName();
+}
+
+class TV extends Controller{
+    private String name="TV";
+    public TV(boolean tf){
+        this.power=tf;
+    }
+    public void show(){
+        if(power==false){
+            System.out.println(name+"가 꺼졌습니다.");
+        }
+        else {
+            System.out.println(name+"가 켜졌습니다.");
+        }
+    }
+    public String getName(){
+        return this.name;
+    }
+}
+
+class Radio extends Controller{
+    private String name="라디오";
+    public Radio(boolean tf){
+        this.power=tf;
+    }
+    public void show(){
+        if(power==false){
+            System.out.println(name+"가 꺼졌습니다.");
+        }
+        else {
+            System.out.println(name+"가 켜졌습니다.");
+        }
+    }
+    public String getName(){
+        return this.name;
+    }
+}
