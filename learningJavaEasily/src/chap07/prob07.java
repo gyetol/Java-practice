41c7dbc6a1bcb82f36966f1f53b5638f79315e41
gyetol <econostein@naver.com>
2020-01-28 21:29:44 +0900

    chap07 problems & solutions

--- learningJavaEasily/src/chap07/prob07.java
+++ learningJavaEasily/src/chap07/prob07.java
@@ -1,4 +1,3 @@
-/*
 package chap07;
 
 public class prob07 {
@@ -18,10 +17,33 @@ public class prob07 {
 
 interface Human{
     void eat();
+    public static void echo(){
+        System.out.println("야호!!!");
+    }
+    public default void print(){
+        System.out.println("인간입니다.");
+    }
 }
 
 class Worker implements Human{
     public void eat(){
         System.out.println("빵을 먹습니다.");
     }
-}*/
+}
+
+class Student implements Human{
+    private int age;
+    public Student(int age){
+        this.age=age;
+    }
+    public void eat(){
+        System.out.println("도시락을 먹습니다.");
+    }
+    public void print(){
+        System.out.println(age+"세의 학생입니다.");
+    }
+}
+
+class Person extends Worker{
+
+}
