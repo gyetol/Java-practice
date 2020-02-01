5e8f517c7231bed14a4b5d0871eec98448f551d9
gyetol <econostein@naver.com>
2020-02-01 22:25:11 +0900

    chap08 problems & solutions

---
+++ learningJavaEasily/src/chap08/prob02.java
@@ -0,0 +1,14 @@
+package chap08;
+
+public class prob02 {
+    public static void main(String[] args) {
+        //Flyable f = ;
+        f.speed();
+        f.height();
+    }
+}
+
+interface Flyable{
+    void speed();
+    void height();
+}
