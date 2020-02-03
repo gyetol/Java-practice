d017b5be6f7d6682328c664a187e0762e795b19e
gyetol <econostein@naver.com>
2020-02-03 22:34:07 +0900

    chap08 problems & solutions

---
+++ learningJavaEasily/src/chap08/prob04.java
@@ -0,0 +1,16 @@
+package chap08;
+
+import java.util.Scanner;
+
+public class prob04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
+
+abstract class Echoer{
+    void start(){
+        System.out.println("시작합니다.");
+    }
+}
