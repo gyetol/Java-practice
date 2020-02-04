5685107353bf583f1dcb65ef1330e721b4ab78bd
gyetol <econostein@naver.com>
2020-02-04 22:43:03 +0900

    chap08 problems & solutions

---
+++ learningJavaEasily/src/chap08/prob05.java
@@ -0,0 +1,17 @@
+package chap08;
+
+public class prob05 {
+    public static void main(String[] args) {
+        Wordable[] m = {
+
+        };
+        for ()
+        {
+            e.word();
+        }
+    }
+}
+
+interface Wordable{
+    void word();
+}
