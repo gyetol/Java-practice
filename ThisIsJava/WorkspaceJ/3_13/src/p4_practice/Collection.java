875313229a9ba372ea7adf155848d209b556e7dc
gyetol <econostein@naver.com>
2020-03-15 17:32:28 +0900

    java practice

---
+++ ThisIsJava/WorkspaceJ/3_13/src/p4_practice/Collection.java
@@ -0,0 +1,44 @@
+package p4_practice;
+
+public class Collection {
+    private int[] base;
+    private int count;
+
+    public Collection(int size){
+        base=new int[size];
+        count=0;
+    }
+
+    public void setBase(int[] base)
+    {
+        this.base=base;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int[] getBase() {
+        return base;
+    }
+    public int getCount(){
+        return count;
+    }
+    public void add(int value){
+        if(count<base.length){
+            base[count]=value;
+            count++;
+        }
+        else{
+            System.out.println("꽉 차서 넣을수가 없습니다.");
+        }
+    }
+    public int getAt(int idx){
+        return base[idx];
+    }
+
+    public Iterator iterator(){
+        return
+    }
+
+}
---
+++ ThisIsJava/WorkspaceJ/3_13/src/p4_practice/Iterator.java
@@ -0,0 +1,19 @@
+package p4_practice;
+
+public class Iterator {
+    private Collection collection;
+    private int cursor;
+
+    public Iterator(Collection collection){
+        this.collection=collection;
+        cursor=0;
+    }
+
+    public boolean hasNext(){
+        if(collection.g)
+    }
+
+    public int next(){
+
+    }
+}
